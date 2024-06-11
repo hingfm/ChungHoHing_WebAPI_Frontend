04aa481bb7584d322aab523cc3d76f21af05c3ee
hohing <hing.filemaker@gmail.com>
2024-06-12 05:13:12 +0800

    Update dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,13 +1,16 @@
 //import React from 'react';
-import Landing from './Landing'
-
+import Landing from "./Landing";
 
 const Dashboard = () => {
-  return (<>
-  <p></p>
-  <h2 style={{color:"#135200",marginLeft:"25px"}}><strong>Dashboard</strong></h2>
-        <Landing />
-  </>)
-}
+  return (
+    <>
+      <p></p>
+      <h2 style={{ color: "#135200", marginLeft: "25px" }}>
+        <strong>Dashboard</strong>
+      </h2>
+      <Landing />
+    </>
+  );
+};
 
 export default Dashboard;
