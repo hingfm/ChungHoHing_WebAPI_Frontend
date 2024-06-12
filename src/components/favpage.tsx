5bfe37622ced24497741fca11a7a1d49fdb22497
hohing <hing.filemaker@gmail.com>
2024-06-12 18:43:59 +0800

    Update favpage

--- src/components/favpage.tsx
+++ src/components/favpage.tsx
@@ -1,14 +1,12 @@
-import 'antd/dist/reset.css';
-import '../App.css';
-import FavCard from './FavCard'
+import "antd/dist/reset.css";
+import FavCard from "./FavCard";
 
 function FavPage() {
   return (
-    <> 
-    <h2 style={{ color: 'green' }}> Welcome to Blog client Demo</h2>     
-     
+    <>
+      <h2 style={{ color: "green" }}> Welcome to Blog client Demo</h2>
       <FavCard />
     </>
-  )
+  );
 }
 export default FavPage;
