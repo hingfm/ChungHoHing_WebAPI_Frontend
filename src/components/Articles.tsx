d2d2e9c388ff317023d3692a63bceefe948a3715
hohing <hing.filemaker@gmail.com>
2024-06-18 00:18:36 +0800

    Add search and filter function on article page

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,7 +1,7 @@
 import "antd/dist/reset.css";
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card, Col, Row, Spin } from "antd";
+import { Card, Col, Input, Row, Select, Spin } from "antd";
 //import articles from './articles.json';
 import { api } from "./common/http-common";
 import axios from "axios";
@@ -10,19 +10,33 @@ import PostIcon from "./posticon";
 import Displaycomment from "./comments";
 
 const Article = () => {
-  const [articles, setArticles] = React.useState(null);
-  const [loading, setLoading] = React.useState(true);
+  const [articles, setArticles] = React.useState([]);
+  const [keywords, setKeywords] = React.useState("");
+  const [region, setRegion] = React.useState(null);
+  const [regions, setRegions] = React.useState([]);
   React.useEffect(() => {
+    axios.get(`${api.uri}/regions`).then((resp) => {
+      setRegions(resp.data);
+    });
+  }, []);
+  const [loading, setLoading] = React.useState(true);
+  const filterData = () => {
     axios
-      .get(`${api.uri}/articles`)
+      .post(`${api.uri}/articles/filter`, {
+        keywords,
+        region,
+      })
       .then((res) => {
         setArticles(res.data);
         localStorage.setItem("a", JSON.stringify(res.data));
       })
       .then(() => {
         setLoading(false);
-      });
-  }, []);
+      }).catch(() => setArticles([]));
+  };
+  React.useEffect(() => {
+    filterData();
+  }, [region, keywords]);
 
   if (loading) {
     const antIcon = <LoadingOutlined style={{ fontSize: 48 }} spin />;
@@ -33,13 +47,38 @@ const Article = () => {
     } else {
       return (
         <>
+          <div className="flex gap-2 justify-center pb-4">
+            <div className="w-1/3">
+              <Input
+                placeholder="type to search"
+                value={keywords}
+                onChange={(e) => setKeywords(e.target.value)}
+              />
+            </div>
+            <div className="w-1/6">
+              <Select
+                allowClear
+                onChange={(val) => {
+                  setRegion(val);
+                }}
+                className="w-full"
+                options={regions.map((r: any) => {
+                  return {
+                    value: r.region,
+                  };
+                })}
+              />
+            </div>
+          </div>
           <Row gutter={[16, 16]} className="px-10">
             {articles &&
               articles.map(({ id, title, imageurl, links }) => (
                 <Col key={id} span={6}>
                   <Card
                     title={title}
-                    cover={<img className="h-48" alt="example" src={imageurl} />}
+                    cover={
+                      <img className="h-48" alt="example" src={imageurl} />
+                    }
                     className="p-4"
                     hoverable
                     actions={[
